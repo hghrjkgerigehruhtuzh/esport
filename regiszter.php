eb668b1c4dcc6c150b0346ead74d5f96eb2adf14
hghrjkgerigehruhtuzh <167965600+hghrjkgerigehruhtuzh@users.noreply.github.com>
2026-01-27 09:28:20 +0100

    regiszter.php

--- regiszter.php
+++ regiszter.php
@@ -2,53 +2,43 @@
 require 'db.php';
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $name = $_POST["name"];
     $email = $_POST["email"];
-    $password = $_POST["password"];
-    $birthdate = $_POST["birthdate"];
     $phone = $_POST["phone"];
+    $message = $_POST["message"];
 
-
+   
     $stmt = $conn->prepare("SELECT id FROM regriszt WHERE email = ?");
-    if (false === $stmt) {
-        die('Előkészítés sikertelen: ' . htmlspecialchars($conn->error));
-    }
-
     $stmt->bind_param("s", $email);
     $stmt->execute();
     $stmt->store_result();
 
     if ($stmt->num_rows > 0) {
-        echo "A megadott email cím már regisztrálva van.";
-        $stmt->close();
-        exit; 
+        die("A megadott email cím már regisztrálva van.");
     }
     $stmt->close();
 
- 
-    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-
-    $stmt = $conn->prepare("INSERT INTO regriszt (email, password, datum, telefonszam) VALUES (?, ?, ?, ?)");
-    
+  
+    $stmt = $conn->prepare("INSERT INTO regriszt (email, telefonszam, message, name) VALUES (?, ?, ?, ?)");
     if (false === $stmt) {
-        die('Beszúrás előkészítése sikertelen: ' . htmlspecialchars($conn->error));
+        die('Beszúrás hiba: ' . htmlspecialchars($conn->error));
     }
 
-  
-    $stmt->bind_param("ssss", $email, $hashed_password, $birthdate, $phone);
+    $stmt->bind_param("ssss", $email, $phone, $message, $name);
 
     if ($stmt->execute()) {
-       
+         header("Location: index.html");
+    exit;
     } else {
-        echo "Hiba történt a regisztráció során: " . $stmt->error;
+        echo "Hiba: " . $stmt->error;
     }
     
     $stmt->close();
     $conn->close();
 } else {
-
-    header("Location: registracio.html");
+    header("Location: index.html");
     exit;
 }
 ?>
+
 
